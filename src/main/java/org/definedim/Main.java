2b1fe581a0b918b0f403a74226e3bb0e2ecedd80
Tony Shang <64829162+TonyNomoney@users.noreply.github.com>
2023-07-25 13:34:11 +0800

    完善文本文件读取器

--- src/main/java/org/definedim/Main.java
+++ src/main/java/org/definedim/Main.java
@@ -1,16 +1,9 @@
 package org.definedim;
 
-import org.definedim.crypto.RustSM2Crypto;
-import org.definedim.exception.NativeLoadingException;
-import org.definedim.file.TextFileReader;
+import org.definedim.file.reader.TextFileReader;
 
 public class Main {
     public static void main(String[] args) {
-        try {
-            TextFileReader tfr = new TextFileReader("build.gradle");
-            System.out.println(tfr.readAll());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        
     }
 }
---
+++ src/main/java/org/definedim/file/reader/TextFileReader.java
@@ -0,0 +1,83 @@
+package org.definedim.file.reader;
+
+import java.io.*;
+
+public class TextFileReader {
+    File textFile;
+    FileInputStream fis;
+    InputStreamReader isr;
+    BufferedReader br;
+
+    public TextFileReader(File _textFile) {
+        textFile = _textFile;
+        try {
+            fis = new FileInputStream(textFile);
+            isr = new InputStreamReader(fis, "UTF-8");
+            br = new BufferedReader(isr);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public TextFileReader(String _textFile) {
+        textFile = new File(_textFile);
+        try {
+            fis = new FileInputStream(textFile);
+            isr = new InputStreamReader(fis, "UTF-8");
+            br = new BufferedReader(isr);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 设置文件读取编码
+     *
+     * @param charsetName
+     */
+    public void setEncoding(String charsetName) {
+        try {
+            isr = new InputStreamReader(fis, charsetName);
+            br = new BufferedReader(isr);
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * 从上次读取结束的地方读取一行,
+     *
+     * @return 一行内容, 若文件结束或失败则为null
+     */
+    public String readLine() {
+        try {
+            String line = br.readLine();
+            return line;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * 从上次读取结束的地方继续读取直到文件结束
+     *
+     * @return 剩余的全部内容, 文件结束则返回空串
+     */
+    public String readAll() {
+        try {
+            //br.reset();
+            String all = "";
+            String line;
+            String lineSeparator = System.lineSeparator();
+            while ((line = br.readLine()) != null) {
+                //process the line
+                all += line + lineSeparator;
+            }
+            return all;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
