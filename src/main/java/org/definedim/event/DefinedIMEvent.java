73c79da4eef0a81c68ca84fd67688d372be46091
Tony Shang <64829162+TonyNomoney@users.noreply.github.com>
2023-07-31 00:04:22 +0800

    为事件类添加一些属性

--- src/main/java/org/definedim/event/DefinedIMEvent.java
+++ src/main/java/org/definedim/event/DefinedIMEvent.java
@@ -1,5 +1,46 @@
 package org.definedim.event;
 
 public class DefinedIMEvent {
-    long timestamp;
+    private long timestamp;
+    private long eventId;
+    private boolean blocked;
+
+    public DefinedIMEvent(long _t, long _id) {
+        timestamp = _t;
+        eventId = _id;
+    }
+
+    /**
+     * 获取时间戳
+     *
+     * @return
+     */
+    public final long getTimestamp() {
+        return timestamp;
+    }
+
+    /**
+     * 获取事件ID
+     *
+     * @return
+     */
+    public final long getEventId() {
+        return eventId;
+    }
+
+    /**
+     * 查询事件是否被阻塞
+     * @return
+     */
+    public final boolean isBlocked() {
+        return blocked;
+    }
+
+    /**
+     * 阻塞该事件
+     */
+    public final void block() {
+        blocked = true;
+    }
+
 }
