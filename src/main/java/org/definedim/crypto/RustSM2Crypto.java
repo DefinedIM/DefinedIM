22dad6eaf2b90a036fa3ec8f6e7f69bea84c842a
Pinghigh <pinghigh24678@outlook.com>
2023-07-31 15:22:52 +0800

    feat: byte array encryption & decryption

--- src/main/java/org/definedim/crypto/RustSM2Crypto.java
+++ src/main/java/org/definedim/crypto/RustSM2Crypto.java
@@ -1,8 +1,7 @@
 package org.definedim.crypto;
 
-import org.definedim.exception.NativeLoadingException;
-
 import java.io.File;
+import org.definedim.exception.NativeLoadingException;
 
 public class RustSM2Crypto {
 
@@ -10,14 +9,14 @@ public class RustSM2Crypto {
         String OS = System.getProperty("os.name").toLowerCase();
         File libFile;
         if (OS.indexOf("windows") >= 0) {
-            libFile = new File("./native/sm2_crypto.dll");
+            libFile = new File("./native/libsm2_crypto.dll");
         } else if (OS.indexOf("linux") >= 0) {
-            libFile = new File("./native/sm2_crypto.so");
+            libFile = new File("./native/libsm2_crypto.so");
         } else {
             throw new NativeLoadingException("RustSM2Crypto: unsupported platform");
         }
         if (libFile.exists()) {
-            //必须使用绝对路径
+            // 必须使用绝对路径
             System.load(libFile.getAbsolutePath());
         } else {
             throw new NativeLoadingException("RustSM2Crypto: could not load lib file " + libFile.getPath());
@@ -30,12 +29,14 @@ public class RustSM2Crypto {
      */
     public native String getBlake3Sum(String str);
 
-    /** generate a new secret key
+    /**
+     * generate a new secret key
      * @return a new secret key (HEX without leading 0x)
      */
     public native String newSecretKey();
 
-    /** get public key by secret key
+    /**
+     * get public key by secret key
      * @param str secret key (HEX without leading 0x)
      * @return a public key (HEX without leading 0x)
      */
@@ -43,15 +44,31 @@ public class RustSM2Crypto {
 
     /**
      * @param pubKey public key (HEX without leading 0x)
-     * @param rawData the data which needs to be encrypted
-     * @return encrypted string
+     * @param rawString the string which needs to be encrypted
+     * @return encrypted string (C1C2C3)
      */
-    public native String encryptString(String pubKey, String rawData);
+    public native String encryptString(String pubKey, String rawString);
 
     /**
      * @param secKey secret key (HEX without leading 0x)
-     * @param encryptedData the data which needs to be decrypted
-     * @return decrypted string
+     * @param encryptedString the string which needs to be decrypted
+     * @return decrypted string (C1C2C3)
      */
-    public native String decryptString(String secKey, String encryptedData);
+    public native String decryptString(String secKey, String encryptedString);
+
+    /**
+     * @param pubKey public key (HEX without leading 0x)
+     * @param rawData the data which needs to be encrypted
+     * @param length length of the data
+     * @return encrypted data array (C1C2C3)
+     */
+    public native byte[] encryptData(String pubKey, byte[] rawData, long length);
+
+    /**
+     * @param secKey secret key (HEX without leading 0x)
+     * @param rawData the data which needs to be decrypted
+     * @param length length of the encrypted data
+     * @return decrypted data array (C1C2C3)
+     */
+    public native byte[] decryptData(String secKey, byte[] encryptedData, long length);
 }
