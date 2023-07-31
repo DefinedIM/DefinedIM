ae3a43c3769126b38b26386cbc6a54fac530898e
Tony Shang <64829162+TonyNomoney@users.noreply.github.com>
2023-07-31 15:37:50 +0800

    fix:lib file path in Windows

--- src/main/java/org/definedim/crypto/RustSM2Crypto.java
+++ src/main/java/org/definedim/crypto/RustSM2Crypto.java
@@ -1,6 +1,7 @@
 package org.definedim.crypto;
 
 import java.io.File;
+
 import org.definedim.exception.NativeLoadingException;
 
 public class RustSM2Crypto {
@@ -8,9 +9,9 @@ public class RustSM2Crypto {
     public RustSM2Crypto() throws NativeLoadingException {
         String OS = System.getProperty("os.name").toLowerCase();
         File libFile;
-        if (OS.indexOf("windows") >= 0) {
-            libFile = new File("./native/libsm2_crypto.dll");
-        } else if (OS.indexOf("linux") >= 0) {
+        if (OS.contains("windows")) {
+            libFile = new File("./native/sm2_crypto.dll");
+        } else if (OS.contains("linux")) {
             libFile = new File("./native/libsm2_crypto.so");
         } else {
             throw new NativeLoadingException("RustSM2Crypto: unsupported platform");
@@ -31,43 +32,45 @@ public class RustSM2Crypto {
 
     /**
      * generate a new secret key
+     *
      * @return a new secret key (HEX without leading 0x)
      */
     public native String newSecretKey();
 
     /**
      * get public key by secret key
+     *
      * @param str secret key (HEX without leading 0x)
      * @return a public key (HEX without leading 0x)
      */
     public native String genPublicKey(String str);
 
     /**
-     * @param pubKey public key (HEX without leading 0x)
+     * @param pubKey    public key (HEX without leading 0x)
      * @param rawString the string which needs to be encrypted
      * @return encrypted string (C1C2C3)
      */
     public native String encryptString(String pubKey, String rawString);
 
     /**
-     * @param secKey secret key (HEX without leading 0x)
+     * @param secKey          secret key (HEX without leading 0x)
      * @param encryptedString the string which needs to be decrypted
      * @return decrypted string (C1C2C3)
      */
     public native String decryptString(String secKey, String encryptedString);
 
     /**
-     * @param pubKey public key (HEX without leading 0x)
+     * @param pubKey  public key (HEX without leading 0x)
      * @param rawData the data which needs to be encrypted
-     * @param length length of the data
+     * @param length  length of the data
      * @return encrypted data array (C1C2C3)
      */
     public native byte[] encryptData(String pubKey, byte[] rawData, long length);
 
     /**
-     * @param secKey secret key (HEX without leading 0x)
-     * @param rawData the data which needs to be decrypted
-     * @param length length of the encrypted data
+     * @param secKey  secret key (HEX without leading 0x)
+     * @param encryptedData the data which needs to be decrypted
+     * @param length  length of the encrypted data
      * @return decrypted data array (C1C2C3)
      */
     public native byte[] decryptData(String secKey, byte[] encryptedData, long length);
